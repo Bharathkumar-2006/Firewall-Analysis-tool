3a5793689ad12e394ee223943d1bc2a240d5b2db
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-01-15 09:35:44 +0530

    modified main function

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import argparse
 
 def send_packets(target,last_port):
     
+    
+def display_result(allowed_packets,allowed_ports):
+    
+    
+def nmap_scan(target,alowed_packets):
+    
+    
+            
 def main():
     parser = argparse.ArgumentParser(description="Firewall Analysis Tool")
     parser.add_argument("target",help="target ip address")
-    parser.add_argument("lat_port",help="last port to scan")
+    parser.add_argument("last_port",help="last port to scan")
     args = parser.parse_args()
     
     allowed_packets,allowed_ports = send_packets(args.target,args.last_port)
     display_result(allowed_packets,allowed_ports)
+    print("\nRunning scan...")
+    nmap_scan(args.target,allowed_packets)
     
     
 if __name__ == "__main__":
