6a015b965358bdb46f7d7d5930f1e2c7edb94213
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-01-17 10:15:40 +0530

    defined the send_packets function

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import scapy.all as scapy
 import argparse
 
-#def send_packets(target,last_port):
+def send_packets(target,last_port):
+    allowed_packets = []
+    allowed_ports = []
     
+    packet_types = ['TCP', 'UDP', 'ICMP']
+    tcp_flags = ['S', 'A', 'F', 'R', 'P', 'U']  #SYN, ACK, FIN, RST, PSH, URG
+
+    for port in range(1, last_port): 
+        for pkt_type in packet_types:
+            if pkt_type == 'TCP':
+                for flag in tcp_flags:
+                    pkt = scapy.IP(dst=target)/scapy.TCP(dport=port, flags=flag)
+                    response = scapy.sr1(pkt, timeout=2, verbose=False)
+                    if response and pkt not in allowed_packets:
+                        allowed_packets.append(pkt)
+                        if port not in allowed_ports:
+                            allowed_ports.append(port)
+            elif pkt_type == 'UDP':
+                pkt = scapy.IP(dst=target)/scapy.UDP(dport=port)
+                response = scapy.sr1(pkt, timeout=2, verbose=False)
+                if response and pkt not in allowed_packets:
+                    allowed_packets.append(pkt)
+                    if port not in allowed_ports:
+                        allowed_ports.append(port)
+            elif pkt_type == 'ICMP':
+                icmp_types = [0, 3, 4, 5, 8, 11, 12]  #Echo Reply,Destination Unreachable, etc.
+                for icmp_type in icmp_types:
+                    pkt = scapy.IP(dst=target)/scapy.ICMP(type=icmp_type)
+                    response = scapy.sr1(pkt, timeout=2, verbose=False)
+                    if response and pkt not in allowed_packets:
+                        allowed_packets.append(pkt)
+            else:
+                continue    
+    return allowed_packets, allowed_ports
     
 def display_result(allowed_packets,allowed_ports):
     print("\nAllowed packets")
@@ -11,7 +43,7 @@ def display_result(allowed_packets,allowed_ports):
     for prt in allowed_ports:
         print(f"-{prt}")
         
-#def nmap_scan(target,alowed_packets):
+#def nmap_scan(target_ip,allowed_packets):
     
     
             
